2839814b2ae950a4554c227fe289dab22011e536
sumanta98 <111751596+sumanta98@users.noreply.github.com>
2023-10-18 00:25:11 +0530

    setup

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
